ddea5e6c74f1702f9f9e5e37eafa981f202871ab
Denver Coneybeare <dconeybe@google.com>
2026-07-07 19:52:26 -0400

    eslint and prettier added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 let count: number = 0;
 
-const button = document.getElementById('counter-btn');
-const display = document.getElementById('counter-val');
+const button = document.getElementById("counter-btn");
+const display = document.getElementById("counter-val");
 
 if (button && display) {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     count += 1;
     display.textContent = count.toString();
   });
