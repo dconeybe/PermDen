807ff84eed2cb74f37ad5433279703a535d4bea2
Denver Coneybeare <dconeybe@google.com>
2026-07-07 19:47:32 -0400

    skeleton added

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let count: number = 0;
+
+const button = document.getElementById('counter-btn');
+const display = document.getElementById('counter-val');
+
+if (button && display) {
+  button.addEventListener('click', () => {
+    count += 1;
+    display.textContent = count.toString();
+  });
+}
