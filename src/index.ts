4438c6c38d3418c24a5987df28f175ceaf68c15e
Denver Coneybeare <dconeybe@google.com>
2026-07-07 20:11:38 -0400

    add firebase init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { signInAnonymously, signOut } from "firebase/auth";
 
 import { firebaseConfig } from "./firebase_config";
 
@@ -6,12 +7,27 @@ initializeApp(firebaseConfig);
 
 let count: number = 0;
 
-const button = document.getElementById("counter-btn");
-const display = document.getElementById("counter-val");
-
-if (button && display) {
-  button.addEventListener("click", () => {
-    count += 1;
-    display.textContent = count.toString();
-  });
+const button = document.getElementById("btnRunTest");
+if (! button) {
+  throw new Error("internal error fpza3g7f8z: button not found");
 }
+
+const display = document.getElementById("textSpan");
+if (! display) {
+  throw new Error("internal error d33vqdc34e: display not found");
+}
+
+async function runTest(): Promise<void> {
+  if (display) {
+    display.textContent = "Test running!";
+  }
+}
+
+let testRunning = false;
+
+button.addEventListener("click", () => {
+  if (! testRunning) {
+    testRunning = true;
+    runTest();
+  }
+});
