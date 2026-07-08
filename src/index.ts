545d180c47a0ed96a08eb8e618754928c0aa2507
Denver Coneybeare <dconeybe@google.com>
2026-07-07 20:33:18 -0400

    app implemented

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,70 @@
 import { initializeApp } from "firebase/app";
-import { signInAnonymously, signOut } from "firebase/auth";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getFirestore, doc, onSnapshot, setLogLevel } from "firebase/firestore";
 
 import { firebaseConfig } from "./firebase_config";
 
-initializeApp(firebaseConfig);
-
-let count: number = 0;
-
-const button = document.getElementById("btnRunTest");
-if (! button) {
-  throw new Error("internal error fpza3g7f8z: button not found");
-}
-
 const display = document.getElementById("textSpan");
-if (! display) {
-  throw new Error("internal error d33vqdc34e: display not found");
+
+function log(message: string) {
+  if (!display) {
+    throw new Error("internal error d33vqdc34e: display not found");
+  }
+  display.textContent += message + "\n";
 }
 
 async function runTest(): Promise<void> {
-  if (display) {
-    display.textContent = "Test running!";
-  }
+  log("Test Started!");
+
+  log("initializeApp()");
+  const app = initializeApp(firebaseConfig);
+
+  log("getAuth()");
+  const auth = getAuth(app);
+
+  let snapshotListenerRegistered = false;
+
+  auth.onAuthStateChanged((user) => {
+    log(`onAuthStateChanged() callback called with user: ${user?.uid}`);
+
+    if (user && !snapshotListenerRegistered) {
+      log("getFirestore()");
+      const firestore = getFirestore(app);
+
+      setLogLevel("debug");
+
+      log("onSnapshot()");
+      const document = doc(firestore, "foo/bar");
+      onSnapshot(document, {
+        next(snapshot) {
+          log(`snapshotListener.next(): snapshot=${snapshot.ref.path}`);
+        },
+        error(e) {
+          log(`snapshotListener.error(): e=${e.message}`);
+        },
+        complete() {
+          log(`snapshotListener.complete()`);
+        },
+      });
+
+      snapshotListenerRegistered = true;
+    }
+  });
+
+  log("signInAnonymously()");
+  await signInAnonymously(auth);
+  log("signInAnonymously() completed");
+}
+
+const button = document.getElementById("btnRunTest");
+if (!button) {
+  throw new Error("internal error fpza3g7f8z: button not found");
 }
 
 let testRunning = false;
 
 button.addEventListener("click", () => {
-  if (! testRunning) {
+  if (!testRunning) {
     testRunning = true;
     runTest();
   }
